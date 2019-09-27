9da8d86fc159b93e6f91e9acd5b46cfaefd47ede
gustavkirkholt <kirkholtgustav@gmail.com>
2019-09-27 09:47:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Her opretter vi en klasse og en metode til at lave en ny terning
+    }
+    public class Die{
+        private final int MAX = 6;             //Max 6 sider
+        private int faceValue;                 //Hvilken side terningen viser
+
+
+
+}
