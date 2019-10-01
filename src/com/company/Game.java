c796b81c3dbdaca59237afc91c14da5ed7bb9c86
unknown <Jona9290@gmail.com>
2019-10-01 13:54:07 +0200

    added color class and colored the text and players

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 //import static jdk.nashorn.internal.objects.Global.print;
 
+
+
 public class Game {
 
     String player1;
@@ -23,17 +25,18 @@ public class Game {
 
     }
 
+
     public void start() {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hvad er spiller 1's navn");
+        System.out.println( Color.ANSI_RED + "Hvad er spiller 1's navn" + Color.ANSI_RESET);
         String player1 = input.nextLine();
 
-        System.out.println("Hvad er spiller 2's navn");
+        System.out.println(Color.ANSI_BLUE + "Hvad er spiller 2's navn" + Color.ANSI_RESET);
         String player2 = input.nextLine();
 
-        System.out.println(player1 + " & " + player2);
+        System.out.println(Color.ANSI_RED + player1 + Color.ANSI_RESET + " & " + Color.ANSI_BLUE + player2 + Color.ANSI_RESET);
 
         //player one enter name
         //player two enter name;
@@ -41,46 +44,52 @@ public class Game {
         // game start
 
         // while(no winner)
+        System.out.println(Color.ANSI_RESET);
         while (p1Score < 40 && p2Score < 40) {
-            System.out.println("Press Enter to continue " + player1);
+            System.out.println(" ");
+            System.out.println("Press Enter to continue " + Color.ANSI_RED + player1 + Color.ANSI_RESET);
             String Nothing1 = input.nextLine();
             int value1 = die1.roll();
             int value2 = die2.roll();
-            System.out.println("You rolled " + value1 + " and " + value2);
+            System.out.println(Color.ANSI_RED + player1 + Color.ANSI_RESET + ", You rolled " + value1 + " and " + value2);
             if (value1 == value2) {
                 p1Score = p1Score - p1Score;
             } else {
                 p1Score = p1Score + value1 + value2;
             }
-            System.out.println(player1 + " " + p1Score);
-            System.out.println("Press Enter to continue " + player2);
+            System.out.println(Color.ANSI_RED + player1 + Color.ANSI_RESET + " " + p1Score);
+            System.out.println(" ");
+            System.out.println("Press Enter to continue " + Color.ANSI_BLUE + player2 + Color.ANSI_RESET);
             String Nothing2 = input.nextLine();
             int value3 = die1.roll();
             int value4 = die2.roll();
-            System.out.println("You rolled " + value3 + " and " + value4);
+            System.out.println(Color.ANSI_BLUE + player2 + Color.ANSI_RESET + ", You rolled " + value3 + " and " + value4);
             if (value3 == value4) {
                 p2Score = p2Score - p2Score;
             } else {
                 p2Score = p2Score + value3 + value4;
 
             }
-            System.out.println(player2 + " " + p2Score);
+            System.out.println(Color.ANSI_BLUE + player2 + Color.ANSI_RESET + " " + p2Score);
                 // next player roll
                 // check if two of the same
                 // If true - reset score
                 // If false - game continues
                 // Add score
                 // is score above max? set winner
-
                 // end player turn
         }
 
         if (p1Score == p2Score){
             System.out.println("Its a Tie");
         } else if(p1Score > p2Score){
-            System.out.println("Congratulations, " + player1 + " you are the winner");
+            System.out.println( Color.ANSI_RED_BACKGROUND);
+            System.out.println(Color.ANSI_RESET + "Congratulations, " + player1 + " you are the winner");
+            System.out.println( Color.ANSI_RED_BACKGROUND);
         } else {
-            System.out.println("Congratulations, " + player2 + " you are the winner");
+            System.out.println(Color.ANSI_BLUE_BACKGROUND);
+            System.out.println(Color.ANSI_RESET + "Congratulations, " + player2 + " you are the winner");
+            System.out.println(Color.ANSI_BLUE_BACKGROUND);
         }
 
     }
